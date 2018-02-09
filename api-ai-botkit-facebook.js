72f6c50442104824690767a5d40e60c72dc84e08
Josh Salwen <jnsalwen@yahoo.com>
2018-02-09 13:08:07 -0800

    update to support latest botkit * removed deprecated middleware * removed message.type == 'user_message' check

--- api-ai-botkit-facebook.js
+++ api-ai-botkit-facebook.js
@@ -42,7 +42,6 @@ function createApiAiProcessing(config) {
   worker.allCallback = [];
 
   worker.middleware = {
-    query: ware(),
     request: ware(),
     response: ware()
   };
@@ -64,70 +63,66 @@ function createApiAiProcessing(config) {
 
   worker.process = function (message, bot) {
     try {
-      if (message.type == 'user_message') {
-        let isEvent = message.event &&
-          typeof message.event === 'object' &&
-          message.event.name;
-        let requestText = decoder.decode(message.text);
-        requestText = requestText.replace("’", "'");
+      let isEvent = message.event &&
+        typeof message.event === 'object' &&
+        message.event.name;
+      let requestText = decoder.decode(message.text);
+      requestText = requestText.replace("’", "'");
 
-        let channel = message.channel;
-        if (!(channel in worker.sessionIds)) {
-          worker.sessionIds[channel] = worker.useStickySessions ?
-            aguid(channel) :
-            aguid();
-        }
-        // get options from message or set as empty
-        let options = message.apiaiOptions || {};
-        options.sessionId = worker.sessionIds[channel];
+      let channel = message.channel;
+      if (!(channel in worker.sessionIds)) {
+        worker.sessionIds[channel] = worker.useStickySessions ?
+          aguid(channel) :
+          aguid();
+      }
+      // get options from message or set as empty
+      let options = message.apiaiOptions || {};
+      options.sessionId = worker.sessionIds[channel];
 
-        worker.middleware.query.run(requestText, options, util.deprecate(function (err, requestText, options) {
-          worker.middleware.request.run(message, requestText, options, bot, function(err, message, query, options, bot) {
-            let request = isEvent ?
-              worker.apiaiService.eventRequest(
-                message.event,
-                options
-              ) :
-              worker.apiaiService.textRequest(
-                query,
-                options
-              )
-            ;
+      worker.middleware.request.run(message, requestText, options, bot, function(err, message, query, options, bot) {
+        let request = isEvent ?
+          worker.apiaiService.eventRequest(
+            message.event,
+            options
+          ) :
+          worker.apiaiService.textRequest(
+            query,
+            options
+          )
+        ;
 
-            request.on('response', (response) => {
-              worker.middleware.response.run(message, response, bot,
-                function(err, message, response, bot) {
-                  if (err) {
-                    console.error(err);
-                  }
-                  worker.allCallback.forEach((callback) => {
+        request.on('response', (response) => {
+          worker.middleware.response.run(message, response, bot,
+            function(err, message, response, bot) {
+              if (err) {
+                console.error(err);
+              }
+              worker.allCallback.forEach((callback) => {
+                callback(message, response, bot);
+              });
+
+              if (isDefined(response.result)) {
+                let action = response.result.action;
+                // set action to null if action is not defined or used
+                action = isDefined(action) && worker.actionCallbacks[action] ?
+                  action : null;
+
+                if (worker.actionCallbacks[action]) {
+                  worker.actionCallbacks[action].forEach((callback) => {
                     callback(message, response, bot);
                   });
-
-                  if (isDefined(response.result)) {
-                    let action = response.result.action;
-                    // set action to null if action is not defined or used
-                    action = isDefined(action) && worker.actionCallbacks[action] ?
-                      action : null;
-
-                    if (worker.actionCallbacks[action]) {
-                      worker.actionCallbacks[action].forEach((callback) => {
-                        callback(message, response, bot);
-                      });
-                    }
-                  }
                 }
-              );
-            });
+              }
+            }
+          );
+        });
 
-            request.on('error', function (error) {
-              console.error(error);
-            });
+        request.on('error', function (error) {
+          console.error(error);
+        });
 
-            request.end();
-          });
-        }, 'middleware.query.use: Use middleware.request.use instead'));
-      }
+        request.end();
+      });
     } catch (err) {
       console.error(err);
     }
